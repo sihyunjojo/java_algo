9513519012093aa3495d89a79ad58cc96a05a541
조시현 <si4018@naver.com>
2024-02-01 09:47:11 +0900

    과제 제출

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr = new int[9];
-    static int[] result = new int[2];
-    static int[] answer = new int[2];
-    static int sum_values = 0;
-    static boolean[] isVisited;
-    public static void main(String args[]) throws Exception {
+    static int[] temp;
+    static boolean[] isvisited;
+    static int n,m;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        String s = br.readLine();
+        n = s.charAt(0)-'0';
+        m = s.charAt(2)-'0';
 
-        sum_values = 0;
-        for (int i = 0; i < 9; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-            sum_values += arr[i];
-        }
-        sum_values -= 100;
+        isvisited = new boolean[n+1];
+        temp = new int[m];
 
-        isVisited = new boolean[9];
-
-        per(0,0,sum_values);
-        for (int i = 0; i < 9; i++){
-            if (answer[0] != i && answer[1] != i){
-                System.out.println(arr[i]);
-            }
-        }
+        per("",0,1);
+        bw.flush();
     }
-
-
-    private static void per(int count, int start, int sum_value){
-        if (count == 2) {
-            if (sum_value == 0){
-                answer = Arrays.copyOf(result,count);
-                return;
-            }
+    private static void per(String str, int count, int start) throws IOException {
+        if (count == m){
+            bw.write(str + "\n");
             return;
         }
-        for (int i = start; i < 9; i++){
-            sum_value -= arr[i];
-            result[count] = i;
-            per(count+1,i+1, sum_value);
-            sum_value += arr[i];
+        for (int i = start; i <= n; i++){
+            per(str + i + " ",count+1, i);
         }
+
     }
 }
