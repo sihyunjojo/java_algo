d4fd7476767a865513279e8b61d8616a50e3762d
조시현 <si4018@naver.com>
2024-02-01 09:46:37 +0900

    feat: n과m (4) 해결

--- algo_01_31/src/bj/B15652.java
+++ algo_01_31/src/bj/B15652.java
@@ -1,4 +1,33 @@
 package bj;
 
+// n과 m (4) 중복 조합
+import java.io.*;
+
 public class B15652 {
+    static int[] temp;
+    static boolean[] isvisited;
+    static int n,m;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        n = s.charAt(0)-'0';
+        m = s.charAt(2)-'0';
+
+        isvisited = new boolean[n+1];
+        temp = new int[m];
+
+        per("",0,1);
+        bw.flush();
+    }
+    private static void per(String str, int count, int start) throws IOException {
+        if (count == m){
+            bw.write(str + "\n");
+            return;
+        }
+        for (int i = start; i <= n; i++){
+            per(str + i + " ",count+1, i);
+        }
+
+    }
 }
