ab78270baaf92bc850eb4ee1cfd1889198df16e8
sihyunjojo <si4018@naver.com>
2024-02-01 20:51:37 +0900

    feat: 해결
    아주 간단한 문제 였으나 누적합 알고리즘을 알면 더 간단하게 풀 수 있는 문제

---
+++ algo_02_01/src/swea/hw/Swea_2001_Best.java
@@ -0,0 +1,51 @@
+package swea.hw;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// 중복합 알고리즘
+public class Swea_2001_Best {
+    public static void main(String args[]) throws Exception {
+        System.setIn(new FileInputStream("./inputFile/input4.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+
+        for (int tc = 1; tc <= t; tc++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int result = 0;
+            int[][] board = new int[n][n+1];
+            for (int i = 0; i < n; i++){
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j < n+1; j++){
+                    board[i][j] = Integer.parseInt(st.nextToken()) + board[i][j-1];
+                }
+            }
+
+            for (int i = 0; i <= n-m; i++){
+                for (int j = 1; j <= n-m+1; j++){
+                    int tmp = 0;
+                    for (int k = i; k < i+m; k++){
+                        tmp += board[k][j+m-1] - board[k][j-1];
+                    }
+                    if (result < tmp) {
+                        result = tmp;
+                    }
+                }
+            }
+
+
+            bw.write("#"+ tc + " " + result+ "\n");
+
+
+        }
+        bw.flush();
+    }
+}
---
+++ algo_02_01/src/swea/hw/Swea_2001_My.java
@@ -0,0 +1,53 @@
+package swea.hw;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// 무식한 4중 포문
+// 원래 나는 더 무식하게 하나의 위치당 4개 9개 이렇게 만든 다음에 옮겨주려고 했음.
+// 그거 보단 덜 무식한듯.
+public class Swea_2001_My {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+
+        for (int tc = 1; tc <= t; tc++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int result = 0;
+            int[][] board = new int[n][n];
+            for (int i = 0; i < n; i++){
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++){
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i <= n-m; i++){
+                for (int j = 0; j <= n-m; j++){
+                    int tmp = 0;
+                    for (int k = i; k < i+m; k++){
+                        for (int l = j; l < j+m; l++){
+                            tmp += board[k][l];
+                        }
+                    }
+                    if (result < tmp) {
+                        result = tmp;
+                    }
+                }
+            }
+
+
+            bw.write("#"+ tc + " " + result+ "\n");
+
+
+        }
+        bw.flush();
+    }
+}
