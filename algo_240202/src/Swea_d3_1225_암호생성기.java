241f70008977e105c20328dfd496417e255d9f27
조시현 <si4018@naver.com>
2024-02-05 17:46:09 +0900

    feat:해결

---
+++ algo_240202/src/Swea_d3_1225_암호생성기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+// String으로 선언한 후 하나씩 접근하는 방법
+public class Swea_d3_1225_암호생성기 {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("inputFile/input6.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int tc = 1; tc <= 10; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+
+            ArrayDeque<Integer> arr = new ArrayDeque<>();
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < 8; i++){
+                arr.add(Integer.valueOf(st.nextToken()));
+            }
+
+            int num = 0, tmp = 0;
+            while (true){
+                num = (num % 5) + 1;
+                tmp = arr.pollFirst() - num;
+                if (tmp <= 0){
+//                    tmp = 0;
+//                    arr.offerLast(tmp);
+                    arr.offerLast(0);
+                    break;
+                }
+                arr.offerLast(tmp);
+            }
+            sb.append("#").append(t);
+            for (int i = 0; i < 8; i++){
+                sb.append(" ").append(arr.pollFirst());
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
