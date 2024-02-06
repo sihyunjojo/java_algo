c259f60e4c757280065764873f8e0b1da216bb8e
sihyunjojo <si4018@naver.com>
2024-02-06 22:59:00 +0900

    Feat: 한빈이와 spotmart해결

--- algo_240206/src/Swea_d3_9229_한빈이와SpotMart.java
+++ algo_240206/src/Swea_d3_9229_한빈이와SpotMart.java
@@ -4,7 +4,7 @@ import java.util.*;
 // 두 합이 하나의 값에 가까까운 값이 될때까지 찾는 문제
 // 투 포인터?? 이진 탐색??
 // 이진트리는 아닌 거 같은데 내가 부족해서 안 보이는 걸 수 도 있겠다.
-public class Solution {
+public class Swea_d3_9229_한빈이와SpotMart {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -17,7 +17,7 @@ public class Solution {
         for (int t = 1; t <= tc; t++){
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken()); // 200 000
+            int m = Integer.parseInt(st.nextToken());
 
             List<Integer> arr = new ArrayList<>();
             st = new StringTokenizer(br.readLine());
@@ -31,7 +31,6 @@ public class Solution {
 
             int start = 0, end = n-1;
             while(start < end){
-                System.out.println(start +" " +end);
                 if (arr.get(start) + arr.get(end) == m){
                     result = m;
                     break;
@@ -42,17 +41,9 @@ public class Solution {
                     ++start;
                 }
                 else{
-                    // 여기는 왜  = 안쓰지?? // 어자피 걸러지니까~
-                    if (arr.get(start) + arr.get(end) < m){
-                        System.out.println("11");
-                        result = Math.max(result, arr.get(start) + arr.get(end));
-                    }
                     --end;
                 }
-                // 아까 전에 왜 다르게 하는걸로 다르지?
-                System.out.println(result);
             }
-            System.out.println(arr);
 
 
             sb.append("#").append(t).append(" ").append(result).append("\n");
--- algo_240206/src/Swea_d3_9229_한빈이와SpotMart_Fail.java
+++ algo_240206/src/Swea_d3_9229_한빈이와SpotMart_Fail.java
@@ -4,11 +4,10 @@ import java.util.*;
 // 두 합이 하나의 값에 가까까운 값이 될때까지 찾는 문제
 // 투 포인터?? 이진 탐색??
 // 이진트리는 아닌 거 같은데 내가 부족해서 안 보이는 걸 수 도 있겠다.
-public class Solution {
+public class Swea_d3_9229_한빈이와SpotMart_Fail {
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("inputFile/input9229.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -31,29 +30,23 @@ public class Solution {
 
             int start = 0, end = n-1;
             while(start < end){
-                System.out.println(start +" " +end);
                 if (arr.get(start) + arr.get(end) == m){
                     result = m;
                     break;
                 }
                 else if (arr.get(start) + arr.get(end) < m){
-                    // 순서를 다르게하면 뭐가 달라지나??
-                    result = Math.max(result, arr.get(start) + arr.get(end));
                     ++start;
-                }
-                else{
-                    // 여기는 왜  = 안쓰지?? // 어자피 걸러지니까~
                     if (arr.get(start) + arr.get(end) < m){
-                        System.out.println("11");
                         result = Math.max(result, arr.get(start) + arr.get(end));
                     }
-                    --end;
                 }
-                // 아까 전에 왜 다르게 하는걸로 다르지?
-                System.out.println(result);
+                else{
+                    --end;
+                    if (arr.get(start) + arr.get(end) < m){
+                        result = Math.max(result, arr.get(start) + arr.get(end));
+                    }
+                }
             }
-            System.out.println(arr);
-
 
             sb.append("#").append(t).append(" ").append(result).append("\n");
         }
