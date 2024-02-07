d10440511f8da1ded9e66f9fbb32ae70e8fe7735
조시현 <si4018@naver.com>
2024-02-07 18:01:33 +0900

    add

---
+++ algo_240207/src/unsolved/Bj_색종이.java
@@ -0,0 +1,6 @@
+package unsolved;
+
+// for문으로만 해결되는 문제긴함.
+// dfs인가 그거 안써도 된데.
+public class Bj_색종이 {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-// 탑
+// 메모리 4mb
+// 함수 만들어서 소수이면 for문 돌려주고 맞으면 sb에 넣어주고
 public class Main {
-    static int[] wheels;
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static boolean[] isso;
     public static void main(String[] args) throws IOException {
-        // n개의 높이가 서로 다른 탑을 수평 직선의 왼쪽부터 오른쪽 방향으로 차례로 세우고
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+       Scanner sc = new Scanner(System.in);
+       n = sc.nextInt();
+       int MAX_N = 8;
+       boolean[] isso = new boolean[(int) Math.pow(10,n+1)];
 
-        wheels = new int[4];
-        for (int i = 0; i < 4; i++){
-            st = new StringTokenizer(br.readLine());
-            wheels[i] = Integer.parseInt(st.nextToken());
-        }
 
-        st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
-
-        int[][] commend = new int[k][2];
-        for (int i = 0; i < k; i++){
-            st = new StringTokenizer(br.readLine());
-            commend[i][0] = Integer.parseInt(st.nextToken());
-            commend[i][1] = Integer.parseInt(st.nextToken());
-        }
-        int result = 0;
-
-        System.out.println(wheels[0]);
-        change(wheels[0],1);
-        System.out.println(wheels[0]);
-        change(wheels[0],2);
-        System.out.println(wheels[0]);
+        System.out.println(sb);
+    }
+    private void fun(){
 
     }
-    // dir는 영향을 주는 wheel이 영향을 받는 wheel을 기준으로 어디 있는지 확인함.
-    // dir = 1 오른쪽
-    private static void go(int wheel1, int wheel2, int dir){
-        if (dir == 1){
-//            wheels[wheel1] <<
-        }
-    }
-    // dir 는 돌아가는 방향
-    // dir = 1 시계 방향
-    private static void change(int wheel_num,int dir){
-        if (dir == 1){
-            int tmp = wheels[wheel_num] / 128;
-            wheels[wheel_num] = wheels[wheel_num] << 1;
-            wheels[wheel_num] += tmp;
-        }
-        if (dir == 2){
-            int tmp = wheels[wheel_num] % 2;
-            wheels[wheel_num] = wheel_num >> 1;
-            wheels[wheel_num] += (128 * tmp);
+
+    private boolean check(int num){
+        for (int i = n; i > 0; i--){
+            if (isso[(int) (num % (Math.pow(10,i)))]) {
+
+            }
         }
     }
 }
