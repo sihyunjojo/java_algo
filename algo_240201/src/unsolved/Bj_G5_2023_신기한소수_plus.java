65cf2c6f154a8e6873d998abf4576f45df97110b
sihyunjojo <si4018@naver.com>
2024-02-09 03:38:48 +0900

    rename

---
+++ algo_240201/src/unsolved/Bj_G5_2023_신기한소수_plus.java
@@ -0,0 +1,32 @@
+package unsolved;
+
+import java.io.*;
+import java.util.*;
+
+// 메모리 4mb
+// 함수 만들어서 소수이면 for문 돌려주고 맞으면 sb에 넣어주고
+public class Bj_G5_2023_신기한소수_plus {
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static boolean[] isso;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int MAX_N = 8;
+//       boolean[] isso = new boolean[(int) Math.pow(10,n+1)];
+
+
+        System.out.println(sb);
+    }
+    private void fun(){
+
+    }
+
+//    private boolean check(int num){
+//        for (int i = n; i > 0; i--){
+//            if (isso[(int) (num % (Math.pow(10,i)))]) {
+//
+//            }
+//        }
+//    }
+}
--- algo_240201/src/unsolved/Bj__2023_신기한소수_plus.java
+++
@@ -1,4 +0,0 @@
-package unsolved;
-
-public class Bj__2023_신기한소수_plus {
-}
