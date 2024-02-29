66b4d4ae286c8d73463826b0f60742e8fd050e03
sihyunjojo <si4018@naver.com>
2024-03-01 08:31:54 +0900

    feat:헤결

--- algo_240229/src/Bj_G3_1941_소문난칠공주.java
+++ algo_240229/src/Bj_G3_1941_소문난칠공주.java
@@ -1,2 +1,120 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
 public class Bj_G3_1941_소문난칠공주 {
+    static class Person{
+        int x,y,index;
+
+        public Person(int y, int x) {
+            this.x = x;
+            this.y = y;
+            this.index = 10 * y + x;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", index=" + index +
+                    '}';
+        }
+    }
+    static char[][] board;
+    static boolean[] isvisited;
+    static int[] personList,xs,ys;
+    static int res;
+    static ArrayList<Person> idasom, imdoyeon;
+    static int[][] delta = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//        idasom = new ArrayList<>();
+//        imdoyeon = new ArrayList<>();
+        board = new char[5][5];
+        for (int i = 0; i < 5; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
+
+
+        // 이다솜파 4명을 끼우고 그다음을 아무나 끼우면 되잖아.
+//        for (int i = 4; i < 7; i++) {
+//            combination(0,0,i);
+//        }
+
+        xs = new int[25];
+        ys = new int[25];
+
+        for (int i = 0; i < 25; i++){
+            xs[i] = i % 5;
+            ys[i] = i / 5;
+        }
+
+        personList = new int[7];
+
+        combination(0,0);
+        System.out.println(res);
+    }
+
+    static void combination(int count, int start){
+        if (count == 7){
+            bfs();
+            return;
+        }
+        for (int i = start; i < 25; i++){
+            personList[count] = i;
+            combination(count+1,i+1);
+        }
+    }
+
+    static void bfs() {
+        int dasom_cnt = 0;
+        int cnt = 0;
+        ArrayDeque<Integer> q = new ArrayDeque();
+        isvisited = new boolean[7];
+
+        q.add(0);
+
+
+        while (!q.isEmpty()){
+            int poll = q.poll();
+            int y = ys[personList[poll]];
+            int x = xs[personList[poll]];
+            for (int d = 0; d < 4; d++){
+                int dy = y + delta[d][0];
+                int dx = x + delta[d][1];
+
+                if (check_size(dy,dx)){
+                    int next = dx + (dy * 5);
+
+                    for (int i = 0; i < 7; i++){
+                        if (personList[i] == next){
+                            if (!isvisited[i]){
+                                isvisited[i] = true;
+                                q.add(i);
+                                cnt++;
+                                if (board[dy][dx] == 'S'){
+                                    dasom_cnt++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (cnt == 7 && dasom_cnt > 3){
+            res++;
+        }
+    }
+
+    static boolean check_size(int y, int x) {
+        return 0 <= y && y < 5 && 0 <= x && x < 5;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -24,77 +24,96 @@ public class Main {
         }
     }
     static char[][] board;
+    static boolean[] isvisited;
+    static int[] personList,xs,ys;
+    static int res;
+    static ArrayList<Person> idasom, imdoyeon;
+    static int[][] delta = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        idasom = new ArrayList<>();
+//        idasom = new ArrayList<>();
+//        imdoyeon = new ArrayList<>();
         board = new char[5][5];
         for (int i = 0; i < 5; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
-                board[i][j] = st.nextToken().charAt(0);
-
-            }
+            board[i] = br.readLine().toCharArray();
         }
 
-        // 이다솜파 4명을 끼우고 그다음을 아무나 끼우면 되잖아.
-        temp_arr = new int[7];
-        combination(0,0);
 
+        // 이다솜파 4명을 끼우고 그다음을 아무나 끼우면 되잖아.
+//        for (int i = 4; i < 7; i++) {
+//            combination(0,0,i);
+//        }
+
+        xs = new int[25];
+        ys = new int[25];
+
+        for (int i = 0; i < 25; i++){
+            xs[i] = i % 5;
+            ys[i] = i / 5;
+        }
+
+        personList = new int[7];
+
+        combination(0,0);
+        System.out.println(res);
     }
 
     static void combination(int count, int start){
-        if (count == 3){
-            isvisited = new boolean[n][m];
+        if (count == 7){
             bfs();
             return;
         }
-        for (int i = start; i < points.size(); i++){
-            temp_arr[count] = i;
+        for (int i = start; i < 25; i++){
+            personList[count] = i;
             combination(count+1,i+1);
         }
     }
 
     static void bfs() {
-        int cnt = 0; // 바이러스가 옮긴 곳. (안전구역이 아닌곳) // 최저가 되야함.
-        ArrayDeque<int[]> q = new ArrayDeque();
+        int dasom_cnt = 0;
+        int cnt = 0;
+        ArrayDeque<Integer> q = new ArrayDeque();
+        isvisited = new boolean[7];
 
-        for (int i = 0; i < 3; i++){
-            Point point = points.get(temp_arr[i]);
-            isvisited[point.y][point.x] = true;
-        }
+        q.add(0);
 
-        for (Point virus : viruss) {
-            q.add(new int[]{virus.y, virus.x});
-            isvisited[virus.y][virus.x] = true;
-        }
 
         while (!q.isEmpty()){
-            int[] poll = q.poll();
-            int y = poll[0];
-            int x = poll[1];
+            int poll = q.poll();
+            int y = ys[personList[poll]];
+            int x = xs[personList[poll]];
             for (int d = 0; d < 4; d++){
                 int dy = y + delta[d][0];
                 int dx = x + delta[d][1];
 
-                if (check_size(dy,dx) && !isvisited[dy][dx] && board[dy][dx] != 1){
-                    isvisited[dy][dx] = true;
-                    q.add(new int[] {dy,dx});
-                    cnt++;
-                    if (cnt >= res) return;
+                if (check_size(dy,dx)){
+                    int next = dx + (dy * 5);
+
+                    for (int i = 0; i < 7; i++){
+                        if (personList[i] == next){
+                            if (!isvisited[i]){
+                                isvisited[i] = true;
+                                q.add(i);
+                                cnt++;
+                                if (board[dy][dx] == 'S'){
+                                    dasom_cnt++;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-//        Point{x=4, y=0, index=4}
-//        Point{x=3, y=1, index=13}
-        res = cnt;
+
+        if (cnt == 7 && dasom_cnt > 3){
+            res++;
+        }
     }
 
     static boolean check_size(int y, int x) {
-        return 0 <= y && y < n && 0 <= x && x < m;
+        return 0 <= y && y < 5 && 0 <= x && x < 5;
     }
 }
 
