3fba1a3b5d299e1d9bf0905f044db69e5794f35a
조시현 <si4018@naver.com>
2024-03-08 09:12:05 +0900

    move

--- algo_study/algo_st_2402_3/src/again/Bj_G3_20058_마법사상어와파이어스톰_Stranger.java
+++ algo_study/algo_st_2402_3/src/again/Bj_G3_20058_마법사상어와파이어스톰_Stranger.java
@@ -1,10 +1,12 @@
+package again;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 
-public class Bj_G3_20058_마법사상어와파이어스톰 {
+public class Bj_G3_20058_마법사상어와파이어스톰_Stranger {
     static int[][] board;
     static int n, two_pow_n;
 
@@ -45,9 +47,10 @@ public class Bj_G3_20058_마법사상어와파이어스톰 {
 
     static void go(int num) {
         turn(num);
-        print_board(board);
+        print_board();
         full_search_minus();
-        print_board(board);
+        print_board();
+        System.out.println(ice_sum());
     }
 
     static void turn(int num) {
@@ -55,49 +58,69 @@ public class Bj_G3_20058_마법사상어와파이어스톰 {
         int[][] temp_board = new int[two_pow_n][two_pow_n];
         System.out.println(num);
         int two_pow = (int) Math.pow(2, num);
+        int two_pow_mini = (int) Math.pow(2, num - 1);
+        int[][] delta = new int[][]{{0, two_pow_mini}, {two_pow_mini, 0}, {-two_pow_mini, 0},{0, -two_pow_mini}};
+
+        boolean zero_flag = false;
+        if (num == 0) return;
 
         for (int i = 0; i < two_pow_n; i = i + two_pow) {
             for (int j = 0; j < two_pow_n; j = j + two_pow) {
 
-                for (int k = i; k < i + two_pow; k++) {
-                    for (int l = j; l < j + two_pow; l++) {
-                        temp_board[i + (l-j) ][j + (two_pow- (k-i) -1)] = board[k][l]; // 이걸 반드시 외워야해
-                        //  [j][size-j-1]= [i][j]
+                // 나눠짐.
+                int[][] temp = new int[4][2];
+                int index = 0;
+                for (int k = i; k < two_pow + i; k += two_pow_mini) {
+                    for (int l = j; l < two_pow + j; l += two_pow_mini) {
+//                        if (!one_flag){
+                        temp[index][0] = k;
+                        temp[index++][1] = l;
+//                    }
+
+//                        if (one_flag) temp_board[k][j+1- l] = board[l][k]; // 외워
                     }
                 }
+
+//                if (!one_flag) {
+                for (int k = 0; k < 4; k++) {
+                    for (int l = temp[k][0]; l < temp[k][0] + two_pow_mini; l++) {
+                        for (int ll = temp[k][1]; ll < temp[k][1] + two_pow_mini; ll++) {
+                            temp_board[l + delta[k][0]][ll + delta[k][1]] = board[l][ll];
+                        }
+                    }
+                }
+//                }
+//                for (int[] ints : temp_board) {
+//                    System.out.println(Arrays.toString(ints));
+//                }
+//                System.out.println();
             }
         }
+
         board = temp_board;
     }
 
     static void full_search_minus() {
-        int[][] new_board = new int[two_pow_n][two_pow_n];
-
         for (int i = 0; i < Math.pow(2, n); i++) {
             for (int j = 0; j < Math.pow(2, n); j++) {
-                if (board[i][j] == 0) continue;
-                new_board[i][j] = minus(i, j);
+                minus(i, j);
             }
         }
-        board = new_board;
     }
 
-    static int minus(int y, int x) {
-
+    static void minus(int y, int x) {
         int[][] delta = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         // 인접해 있는 칸 중에서 얼음이 3개나 4개가 있지 않으면, 얼음이 한칸 씩 줄어든다.
         int ice_block = 0;
         for (int d = 0; d < 4; d++) {
             int dy = y + delta[d][0];
             int dx = x + delta[d][1];
-            if (check_out_of_index(dy, dx) && board[dy][dx] > 0) {
+            if (check_out_of_index(dy, dx) && board[dy][dx] != 0) {
                 ice_block += 1;
             }
         }
         if (ice_block < 3) {
-            return board[y][x] - 1;
-        } else {
-            return board[y][x];
+            board[y][x] -= 1;
         }
     }
 
@@ -109,7 +132,7 @@ public class Bj_G3_20058_마법사상어와파이어스톰 {
         int sum = 0;
         for (int i = 0; i < two_pow_n; i++) {
             for (int j = 0; j < two_pow_n; j++) {
-                if (board[i][j] > 0) sum += board[i][j];
+                sum += board[i][j];
             }
         }
         return sum;
@@ -123,7 +146,7 @@ public class Bj_G3_20058_마법사상어와파이어스톰 {
 
         for (int i = 0; i < two_pow_n; i++){
             for (int j = 0; j < two_pow_n; j++){
-                if (isvisited[i][j] || board[i][j] < 0) continue;
+                if (isvisited[i][j] || board[i][j] == 0) continue;
                 int tmp_sum = 0;
                 Deque<int[]> q = new ArrayDeque<>();
                 q.addFirst(new int[] {i,j});
@@ -135,7 +158,10 @@ public class Bj_G3_20058_마법사상어와파이어스톰 {
                     for (int d = 0; d < 4; d++){
                         int dy = y + delta[d][0];
                         int dx = x + delta[d][1];
-                        if (!check_out_of_index(dy,dx) || isvisited[dy][dx] || board[dy][dx] <= 0) continue;
+                        if (!check_out_of_index(dy,dx) || isvisited[dy][dx] || board[dy][dx] == 0) continue;
+//                        if (!check_out_of_index(dy,dx)) continue;
+//                        if (isvisited[dy][dx]) continue;
+//                        if (board[dy][dx] == 0) continue;
                         tmp_sum += 1;
                         isvisited[dy][dx] = true;
                         q.addFirst(new int[] {dy,dx});
@@ -149,7 +175,7 @@ public class Bj_G3_20058_마법사상어와파이어스톰 {
         return final_sum;
     }
 
-    static void print_board(int[][] board) {
+    static void print_board() {
         for (int[] ints : board) {
             System.out.println(Arrays.toString(ints));
         }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-<<<<<<< HEAD
     static class Fish{
         int x,y,index,dir;
 
@@ -120,69 +119,5 @@ public class Main {
         return 0 <= x && x < 4 && 0 <= y && y < 4;
     }
 
-
-=======
-   static class Node {
-      int from,to,w;
-
-      public Node(int from, int to, int w) {
-         this.from = from;
-         this.to = to;
-         this.w = w;
-      }
-   }
-   static StringBuilder sb = new StringBuilder();
-   static int v,e;
-   static long res;
-   static ArrayList<int[]>[] arr;
-
-
-   public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st;
-
-       st = new StringTokenizer(br.readLine());
-
-       v = Integer.parseInt(st.nextToken());
-       e = Integer.parseInt(st.nextToken());
-
-       arr = new ArrayList[v+1];
-
-      for (int i = 0; i < v+1; i++) {
-         arr[i] = new ArrayList<>();
-      }
-
-
-      int from = 0;
-       for (int i = 0; i < e; i++){
-          st = new StringTokenizer(br.readLine());
-          from = Integer.parseInt(st.nextToken());
-          int to = Integer.parseInt(st.nextToken());
-          int w = Integer.parseInt(st.nextToken());
-          arr[from].add(new int[] {to,w});
-          arr[to].add(new int[] {from,w});
-       }
-
-      res = 0;
-      boolean[] isvisited = new boolean[v+1];
-
-      PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-      pq.offer(new int[] {from, 0});
-
-      while (!pq.isEmpty()){
-         int[] poll = pq.poll();
-
-          if(isvisited[poll[0]]) continue;
-          isvisited[poll[0]] = true;
-          res += poll[1];
-
-          for (int[] e : arr[poll[0]]) {
-              if(isvisited[e[0]]) continue;
-              pq.add(e);
-          }
-      }
-       System.out.println(res);
-   }
->>>>>>> 55be9c293e926c5768eb372199fc4449c47aef8c
 }
 
