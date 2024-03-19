0d8e12514b23cfeb4c062ca5d119a64cd8241216
조시현 <si4018@naver.com>
2024-03-19 18:01:10 +0900

    240319

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import javax.swing.text.html.ListView;
-import java.awt.desktop.QuitEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +5,7 @@ import java.util.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
+    static int res;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,47 +15,10 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0 ; i < n; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
 
-        // 무조건 -1 이랑 +1해주는게 조은가?
-        int left = -1;
-        int right = arr[n - 1] + 1;
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-//            System.out.println("mid = " + mid);
-            int sum = 0;
-            int i = 0; int j = 1;
-
-            while (true) {
-//                System.out.println("I" + i + "j " + j);
-                if (arr[j] - arr[i]  >= mid){
-                    i = j;
-                    j = i + 1;
-                    sum++;
-                } else {
-                    j++;
-                }
-                if (j == n) break;
-            }
-
-//            System.out.println("sum = " + sum);
-
-            if (sum >= m-1) {
-                // 이거는 고정을 한데.
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-
-//            System.out.println("left = " + left);
-//            System.out.println("right = " + right);
-        }
-
-        System.out.println(right-1);
     }
 }
