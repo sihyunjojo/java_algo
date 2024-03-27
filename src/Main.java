e96516321b00e1c9721c69ec9ee98b334905d8b7
sihyunjojo <si4018@naver.com>
2024-03-28 01:49:17 +0900

    240328

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-import static java.lang.System.in;
+public class Main{
+    static int[][] sdo;
+    static ArrayList<int[]> req = new ArrayList<>();
 
-public class Main {
-    static int res, n, m;
-    static StringBuilder sb;
-    static char[][] initboard;
-    static int[] start;
-    static int[][] delta = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        sdo = new int[9][9];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-        initboard = new char[9][9];
-
-        for (int i = 0; i < 9; i++) {
-            initboard[i] = br.readLine().toCharArray();
+        for(int i=0;i<9;i++){
+            String s = br.readLine();
+            for(int j=0;j<9;j++){
+                sdo[i][j] = s.charAt(j) - '0';
+                if(sdo[i][j] == 0) req.add(new int[] {i,j});
+            }
         }
-        go();
+
+        dfs(0);
     }
 
-    static void go(){
+    static boolean dfs(int cnt){
+        if(cnt >= req.size()){
+            if (cnt == req.size()) print_sdo();
+            return true;
+        }
 
+        int y = req.get(cnt)[0];
+        int x = req.get(cnt)[1];
+        for(int j=1;j<=9;j++){
+            if(isNum(y,x,j)){
+                sdo[y][x] = j;
+                if (dfs(cnt+1)) return true;
+                sdo[y][x] = 0;
+            }
+        }
+        return false;
+    }
+
+    static boolean isNum(int y, int x, int num){
+        int y_start = (y/3)*3;
+        int x_start = (x/3)*3;
+
+        for(int i=0;i<9;i++){
+            // 가로 기준 탐색
+            if(sdo[y][i]==num) return false;
+
+            // 세로 기준 탐색
+            if(sdo[i][x]==num) return false;
+
+            // 사각형 기준 탐색
+            if(sdo[y_start + i/3][x_start + i%3]==num) return false;
+        }
+        return true;
+    }
+
+
+    static void print_sdo(){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<9;i++){
+            for (int n: sdo[i]) {
+                sb.append(n);
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
     }
 }
