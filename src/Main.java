bd93f4f8326641c826d540faac6ef8e8ea439908
sihyunjo <si4018@naver.com>
2024-04-03 17:57:58 +0900

    240403

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,60 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Main {
-    static class Queen {
-        int x, y, minus, plus, index, in;
-
-        public Queen(int x, int y,int index) {
-            this.x = x;
-            this.y = y;
-            this.index = index;
-            this.minus = x - y + n-1;
-            this.plus = x + y;
-        }
-
-        @Override
-        public String toString() {
-            return "Queen{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
-        }
-    }
-
-    static StringBuilder sb = new StringBuilder();
-    static int[][] board, tempBoard;
-    static int n,res1,res2;
-    static ArrayList<Queen> list1,list2,now_list;
-
+public class Main{
+    static int res;
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
 
-        board = new int[n][n];
-        tempBoard = new int[n][n];
-        list1 = new ArrayList<>();
-        list2 = new ArrayList<>();
-        now_list = new ArrayList<>();
-        int cnt = 0;
-
-        int index = 1;
-        for (int i = 0; i < n; i++) {
+        int tc = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tc; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 1) {
-                    Queen queen = new Queen(j, i, ++index);
-                    if ((i+j) % 2 == 0){
-                        list1.add(queen);
-                    } else {
-                        list2.add(queen);
-                    }
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int[] arr = new int[n+1];
+            for (int j = 1; j < n; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            ArrayList<Integer>[] list = new ArrayList[n+1];
+            int[] condition_make = new int[n+1];
+            for (int j = 0; j < n + 1; j++) {
+                list[j] = new ArrayList<>();
+            }
+
+            int[] dis = new int[n+1];
+            for (int j = 0; j < k; j++) {
+                st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                list[from].add(to);
+
+                // 건물 조건들 저장.. 음.. 그냥 가장 큰거 저장 해도 되려나?
+                condition_make[to] = Math.max(condition_make[to],arr[from]);
+                dis[to]++;
+            }
+
+            int end = Integer.parseInt(br.readLine());
+
+            boolean[] ismake = new boolean[n+1];
+            ArrayDeque<Integer> q = new ArrayDeque<>();
+            for (int j = 1; j < n + 1; j++) {
+                if(dis[j] == 0){
+                    q.add(j);
                 }
             }
-        }
-    }
 
-
-    static boolean checkThis(Queen q) {
-        for (Queen queen : now_list) {
-            if (queen.minus == q.minus) return false;
-            if (queen.plus == q.plus) return false;
-        }
-        return true;
-    }
-
-    static void copyBoard() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                tempBoard[i][j] = board[i][j];
+            while (!q.isEmpty()){
+                Integer now = q.poll();
             }
+            // 같은 게 있다면 둘 중에 비교해서 더 큰거 넣어줌.
         }
     }
+
 }
