7bbae4f7d62bb44cca56bc590d0fe4179cb8ab3d
sihyunjojo <si4018@naver.com>
2024-04-05 00:39:58 +0900

    최장은 다익스트라 왜 안되는지 궁금해

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,75 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
-    static int res;
-    static StringBuilder sb;
+public class Main {
+    static int n, end;
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer>[] list;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+//        System.out.println();
         int tc = Integer.parseInt(br.readLine());
         for (int i = 0; i < tc; i++) {
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            int[] arr = new int[n+1];
-            for (int j = 1; j < n; j++) {
+            arr = new int[n + 1];
+            for (int j = 1; j < n + 1; j++) {
                 arr[j] = Integer.parseInt(st.nextToken());
             }
 
-            ArrayList<Integer>[] list = new ArrayList[n+1];
-            int[] condition_make = new int[n+1];
+            list = new ArrayList[n + 1];
+            int[] condition_make = new int[n + 1];
             for (int j = 0; j < n + 1; j++) {
                 list[j] = new ArrayList<>();
             }
 
-            int[] dis = new int[n+1];
+            int[] dis = new int[n + 1];
             for (int j = 0; j < k; j++) {
                 st = new StringTokenizer(br.readLine());
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
                 list[from].add(to);
-
-                // 건물 조건들 저장.. 음.. 그냥 가장 큰거 저장 해도 되려나?
-                condition_make[to] = Math.max(condition_make[to],arr[from]);
                 dis[to]++;
             }
 
-            int end = Integer.parseInt(br.readLine());
+            end = Integer.parseInt(br.readLine());
 
-            boolean[] ismake = new boolean[n+1];
-            ArrayDeque<Integer> q = new ArrayDeque<>();
-            for (int j = 1; j < n + 1; j++) {
-                if(dis[j] == 0){
-                    q.add(j);
+            Queue<int[]> q = new ArrayDeque<>();
+
+            for (int j = 1; j < n+1; j++) {
+                if (dis[j] == 0){
+                    q.add(new int[] {j});
                 }
             }
 
-            while (!q.isEmpty()){
-                Integer now = q.poll();
-            }
-            // 같은 게 있다면 둘 중에 비교해서 더 큰거 넣어줌.
-        }
-    }
+            System.out.println();
+            int[] dp = new int[n+1];
+            boolean[] isvisited = new boolean[n+1];
 
+            while (!q.isEmpty()){
+                int[] now = q.poll();
+                int now_point = now[0];
+
+                System.out.println(Arrays.toString(now));
+//                if (isvisited[now_point]) continue;
+//                isvisited[now_point] = true;
+
+                for (int next : list[now_point]) {
+                    if (dp[next] >= arr[now_point] + dp[now_point]) continue;
+                    dp[next] = arr[now_point] + dp[now_point];
+                    q.add(new int[] {next});
+                    System.out.println(Arrays.toString(dp));
+                }
+            }
+
+            sb.append(dp[end]+arr[end]).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
