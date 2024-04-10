7135ad3ff241a5c3f52af9925318b7b3e77d067f
sihyunjojo <si4018@naver.com>
2024-04-10 18:14:37 +0900

    0410

--- src/Main1.java
+++ src/Main1.java
@@ -3,113 +3,13 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main1 {
-    static int r, c;
-    static String[][] map;
-    static boolean[][] visited, v;
-    static ArrayList<int[]> person = new ArrayList<>();
-    static Queue<int[]> q = new ArrayDeque<>();
-    static Queue<int[]> q2 = new ArrayDeque<>();
-    static int[] dr = { -1, 1, 0, 0 }; // 상 하 좌 우
-    static int[] dc = { 0, 0, -1, 1 };
-
-    static boolean isPossibe(int x, int y) {
-        if (x < 0 || x >= r)
-            return false;
-        if (y < 0 || y >= c)
-            return false;
-
-        return true;
-    }
-
-    static boolean meet() {
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            for (int d = 0; d < 4; d++) {
-                int nextr = cur[0] + dr[d];
-                int nextc = cur[1] + dc[d];
-
-                if (!isPossibe(nextr, nextc) || v[nextr][nextc])
-                    continue;
-
-                v[nextr][nextc] = true;
-                if(map[nextr][nextc].equals("X")){
-
-                    q2.add(new int[] { nextr, nextc });
-                    continue;
-                }
-
-
-                if (nextr == person.get(1)[0] && nextc == person.get(1)[1])
-                    return true;
-
-
-
-                q.add(new int[] { nextr, nextc });
-            }
-        }
-        q = q2;
-        q2 = new ArrayDeque<>();
-        return false;
-    }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[r][c];
-        v = new boolean[r][c];
-        Queue<int[]> land = new ArrayDeque<>();
-        map = new String[r][c];
-        for (int i = 0; i < r; i++) {
-            String[] s = br.readLine().split("");
-            for (int j = 0; j < c; j++) {
-                map[i][j] = s[j];
-
-                if ("L".equals(map[i][j])) {
-                    land.add(new int[] { i, j });
-                    person.add(new int[] { i, j });
-                }
-
-                if (".".equals(map[i][j])) {
-                    land.add(new int[] { i, j });
-                    visited[i][j] = true;
-                }
-            }
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.println(arr[i]);
+            i++;
         }
-
-        q.add(person.get(0));
-        v[person.get(0)[0]][person.get(0)[1]] = true;
-
-        int day = 0;
-        while (true) {
-            day++;
-            int size = land.size();
-            for (int i = 0; i < size; i++) {// 땅 주위 빙하 녹이기
-                int[] cur = land.poll();
-                for (int d = 0; d < 4; d++) {
-                    int nextr = cur[0] + dr[d];
-                    int nextc = cur[1] + dc[d];
-
-                    if (!isPossibe(nextr, nextc) || visited[nextr][nextc] || !map[nextr][nextc].equals("X"))
-                        continue;
-
-                    visited[nextr][nextc] = true;
-                    map[nextr][nextc] = ".";
-                    land.add(new int[] { nextr, nextc });
-                }
-            }
-
-            if (meet()) { // 두사람 만나는지 확인
-                break;
-            }
-        }
-
-        System.out.println(day);
     }
 }
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,74 @@
+import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static HashMap<Long, Long> f;
-    static long start, end, ans;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        f = new HashMap<Long, Long>();
+    static class Node implements Comparable<Node> {
+        int x;
+        int y;
+        int cost;
 
-        long sum = 0;
-        for(long i=0; i<10; i++) {
-            sum += i;
-            f.put(i, sum);
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
         }
 
-        for(int t=1; t<=tc; t++) {
-            start = sc.nextLong();
-            end = sc.nextLong();
+        @Override
+        public int compareTo(Node o) {
+            return cost - o.cost;
+        }
+    }
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int[][] delta = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
 
-            if(start > 0)
-                ans = F(end) - F(start-1);
-            else
-                ans = F(end) - F(start);
-            sb.append("#").append(t).append(" ").append(ans).append("\n");
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("InputFile/input1249.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= tc; t++) {
+            sb.append("#").append(t).append(" ");
+
+            n = Integer.parseInt(br.readLine());
+
+            int[][] board = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                String s = br.readLine();
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = s.charAt(j) - '0';
+                }
+            }
+
+            boolean[][] isvisited = new boolean[n][n];
+
+            PriorityQueue<Node> pq = new PriorityQueue<>();
+            pq.add(new Node(0,0,0));
+            isvisited[0][0] = true;
+
+
+            loop:
+            while (!pq.isEmpty()) {
+                Node now = pq.poll();
+
+                for (int d = 0; d < 4; d++) {
+                    int y = now.y + delta[d][0];
+                    int x = now.x + delta[d][1];
+                    if (!checkSize(y, x)) continue;
+                    if (isvisited[y][x]) continue;
+                    isvisited[y][x] = true;
+                    pq.add(new Node(x,y,board[y][x] + now.cost));
+
+                    if (y == n - 1 && x == n - 1) {
+                        sb.append(now.cost).append("\n");
+                        break loop;
+                    }
+                }
+            }
         }
         System.out.println(sb);
     }
 
-    static long F(long i) {
-        if(f.containsKey(i)) return f.get(i);
-
-        if(i<10) return f.get(i);
-
-        long v = V(i); // 자리 단위 100, 1000
-        long F = F(i-1-i%v); // 가장 가까운 9 , 99, 999
-        long G = (i/v)*(i%v+1)+ F(i%v); // 100 ~ i
-        long num = F+G; // 최종 값을 더해서 알려줌.
-
-        f.put(i, num);
-
-        return num;
-    }
-
-    static long V(long i) {
-        long v = 1;
-        while(i>=10) {
-            v = v*10;
-            i = i/10;
-        }
-        return v;
+    static boolean checkSize(int y, int x) {
+        return 0 <= y && y < n && 0 <= x && x < n;
     }
 }
-
-
-
