be591646bd83e46e72d3849035ee23df8ea587c1
sihyunjojo <si4018@naver.com>
2024-04-13 02:06:41 +0900

    0413

--- src/Main.java
+++ src/Main.java
@@ -1,195 +1,44 @@
-<<<<<<< HEAD
-=======
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
-    static int res,n,m,tmp_res;
-    static StringBuilder sb;
-    static char[][] board;
-    static int[] blue, red, first, second, tmp_dirs,init_red,init_blue;
-    static boolean isRedFirst;
-    static int[][] delta = new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
+public class Main {
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        board = new char[n][m];
-        blue = new int[3];
-        init_blue = new int[3];
-        red = new int[3];
-        init_red = new int[3];
-        first = new int[3];
-        second = new int[3];
-        tmp_dirs = new int[10];
+        int[] in = new int[n+1];
 
-        for (int i = 0; i < n; i++) {
-            board[i] = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'B'){
-                    blue[0] = i;
-                    blue[1] = j;
-                    // 추후에 순서에 따라서 나눌때 파란색 체크하기 위해서
-                    blue[2] = 0;
-                    init_blue[0] = i;
-                    init_blue[1] = j;
-                    init_blue[2] = 0;
-
-                }
-                if (board[i][j] == 'R'){
-                    red[0] = i;
-                    red[1] = j;
-                    red[2] = 1;
-                    init_red[0] = i;
-                    init_red[1] = j;
-                    init_red[2] = 1;
-
-                }
-            }
+        // n개의 문제를 전부 풀어야함.
+        // 1번 문제가 가장 쉬운 문제임. -> 같은 조건일때는 숫자가 작은게 먼저 나와야함.
+        ArrayList<Integer>[] list = new ArrayList[n+1];
+        for (int i = 0; i < n+1; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            // 진입 차수 계산
+            in[to]++;
+            list[from].add(to);
         }
 
-        res = 11;
-        combi(0);
-        if (res == 11) System.out.println(-1);
-        else System.out.println(res);
-    }
-
-    private static void combi(int count) {
-        if (count == 10) {
-            red[0] = init_red[0];
-            red[1] = init_red[1];
-            blue[0] = init_blue[0];
-            blue[1] = init_blue[1];
-            //[2, 1, 3, 1, 2]
-//            System.out.println(Arrays.toString(tmp_dirs));
-            go();
-            return;
+        for (int i = 1; i < n+1; i++) {
+            list[i].sort(Comparator.naturalOrder());
         }
 
-        for (int i = 0; i < 4; i++) {
-            tmp_dirs[count] = i;
-            combi(count+1);
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i < n + 1; i++) {
+            if (in[i] == 0) q.add(i);
         }
 
-    }
-
-    private static void go() {
-        tmp_res = 100;
-        int[] target = {2, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-
-        for (int i = 0; i < 10; i++) {
-            whosFirst(tmp_dirs[i]);
-            // 파란색이 들어가거나, 안되는 상황이면 false를 넘겨주자.
-            if(!moveBalls(i)) return;
-            if (res <= i) return;
-//            if (same(tmp_dirs,target)){
-//                System.out.println("true");
-//            }
-        }
-    }
-
-    private static void whosFirst(int dir) {
-        switch (dir){
-            case 0:
-                if (red[0] > blue[0]) isRedFirst = false;
-                else  isRedFirst = true;
-                break;
-            case 1:
-                if (red[0] < blue[0]) isRedFirst = false;
-                else  isRedFirst = true;
-                break;
-            case 2:
-                if (red[1] < blue[1]) isRedFirst = true;
-                else  isRedFirst = false;
-                break;
-            case 3:
-                if (red[1] > blue[1]) isRedFirst = true;
-                else  isRedFirst = false;
-                break;
-        }
-        if (isRedFirst) {
-            first = red;
-            second = blue;
-        } else {
-            first = blue;
-            second = red;
-        }
-    }
-
-
-    private static boolean moveBalls(int index) {
-        int[] target = {2, 3, 3, 3, 3, 3, 3, 3, 3, 3};
-
-        if (!moveBall(index, first)) return false;
-        if (!moveBall(index, second)) return false;
-        // 별일 없이 일 마침.
-
-        if (res > tmp_res) res = tmp_res;
-        return true;
-    }
-
-    private static boolean moveBall(int index, int[] ball) {
-        int[] target = {2, 3, 3, 3, 3, 3, 3, 3, 3, 3};
-//        System.out.println(Arrays.toString(target));
-        int dir = tmp_dirs[index];
-
-        int ball_y = ball[0];
-        int ball_x = ball[1];
-        int ball_color = ball[2];
-        while (true){
-            ball_y += delta[dir][0];
-            ball_x += delta[dir][1];
-
-//            if (same(tmp_dirs,target)){
-//                System.out.println(ball_y + " "+ ball_x + " " + Arrays.toString(ball));
-//            }
-
-            if(ball[2] == 0 && board[ball_y][ball_x] == 'O'){
-//                System.out.println("false");
-                return false;
-            }
-
-            if(ball == second && first[0] == ball_y && first[1] == ball_x){
-                ball_y -= delta[dir][0];
-                ball_x -= delta[dir][1];
-                ball[0] = ball_y;
-                ball[1] = ball_x;
-                break;
-            }
-            if (board[ball_y][ball_x] == '#'){
-                ball_y -= delta[dir][0];
-                ball_x -= delta[dir][1];
-                ball[0] = ball_y;
-                ball[1] = ball_x;
-                break;
-            }
-
-            if(ball[2] == 1 && board[ball_y][ball_x] == 'O'){
-//                System.out.println(Arrays.toString(tmp_dirs));
-                tmp_res = index+1;
-                ball[0] = ball_y;
-                ball[1] = ball_x;
-                return true;
-            }
+        // 위상정렬하면됨.
+        while (!q.isEmpty()) {
 
         }
-//        if (same(tmp_dirs,target)){
-//            System.out.println("true");
-//        }
-        return true;
-    }
-    private static boolean same(int[] a, int[] b){
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-        return true;
+
     }
 }
-
-//
->>>>>>> a74331bfccf58df519906425910ce3752a0a4e3d
---
+++ unsolved_algo/src/Bj_P5_행성터널.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_P5_행성터널 {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+        }
+    }
+}
