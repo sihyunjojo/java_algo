1b7984aa36916b2f9c3fa7364183cf188a2a622f
sihyunjo <si4018@naver.com>
2024-04-30 17:58:09 +0900

    240430

--- src/Main.java
+++ src/Main.java
@@ -1,230 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int min, max;
     static StringBuilder sb = new StringBuilder();
-    static int[][] arr;
-    static int n, k, underLine;
-    static int[][] delta = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
+    static int[] temp = new int[3];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-
-        st = new StringTokenizer(br.readLine());
-        arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            arr[0][i] = Integer.parseInt(st.nextToken());
-        }
-
-        int time = 0;
-        while (checkFinish()) {
-            underLine = n;
-            time++;
-            solve();
-        }
-
-
-        System.out.println(time);
+        String s = br.readLine();
+        int n = calHolsu(s);
+        max = n;
+        min = n;
+        solve(s);
+        System.out.println(min + " " + max);
     }
 
-
-    static void solve() {
-        init();
-        while (moveUp()) {
-        }
-        moveFish();
-        moveLine();
-        moveHarp();
-        moveFish();
-        moveLine();
-    }
-
-    static void init() {
-        ArrayDeque<Integer> integers = minFish();
-        for (Integer integer : integers) {
-            arr[0][integer]++;
-        }
-
-        arr[1][0] = arr[0][0];
-        for (int i = 0; i < n - 1; i++) {
-            arr[0][i] = arr[0][i + 1];
-        }
-        arr[0][n - 1] = 0;
-
-    }
-
-    static ArrayDeque<Integer> minFish() {
-        ArrayDeque<Integer> list = new ArrayDeque<>();
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            if (min > arr[0][i]) {
-                min = arr[0][i];
-                list.clear();
+    static void solve(String s) {
+        while (s.length() > 1) {
+            if (s.length() == 2) {
+                s = isLenTwo(s);
             }
-            if (min == arr[0][i]) {
-                list.add(i);
+            else {
+                s = isLenUpTwo(s);
             }
-        }
-        return list;
-    }
-
-    static boolean moveUp() {
-        int tmp = 0;
-        for (int i = 0; i < underLine; i++) {
-            if (arr[1][i] == 0) {
-                break;
-            }
-            tmp = i;
-        }
-
-        int maxHeight = 0;
-        for (int j = 0; j < n; j++) {
-            if (arr[j][0] == 0) {
-                break;
-            }
-            if (maxHeight < j) {
-                maxHeight = j;
-            }
-        }
-
-        if (2 * (maxHeight + 1) > underLine) {
-            return false;
-        }
-
-        int[][] tmp_board = new int[n][n];
-
-        // y
-        for (int i = 0; i <= maxHeight; i++) {
-            // x
-            for (int j = 0; j <= tmp; j++) {
-                tmp_board[tmp - j][i] = arr[i][j];
-//                arr[maxHeight-j][i] = arr[i][j];
-            }
-        }
-
-
-        for (int i = 0; i <= tmp; i++) {
-            // x
-            for (int j = 0; j <= maxHeight; j++) {
-                arr[1 + i][j] = tmp_board[i][j];
-//                arr[maxHeight + i][j] = tmp_board[i][j];
-            }
-        }
-
-
-        for (int i = tmp + 1; i < underLine; i++) {
-            arr[0][i - tmp - 1] = arr[0][i];
-        }
-
-        underLine -= tmp + 1;
-        for (int i = underLine; i < n; i++) {
-            arr[0][i] = 0;
-        }
-        return true;
-    }
-
-    static void moveFish() {
-        int[][] tmpBoard = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] == 0) continue;
-                for (int d = 0; d < 4; d++) {
-                    int dy = i + delta[d][0];
-                    int dx = j + delta[d][1];
-                    if (!checkSize(dy, dx)) continue;
-                    if (arr[dy][dx] == 0) continue;
-                    if (arr[i][j] > arr[dy][dx]) {
-                        int mock = (arr[i][j] - arr[dy][dx]) / 5;
-                        if (mock > 0) {
-                            tmpBoard[dy][dx] += mock;
-                            tmpBoard[i][j] -= mock;
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] += tmpBoard[i][j];
-            }
+            int cnt = calHolsu(s);
+            if (cnt > max) max = cnt;
+            if (cnt < min) min = cnt;
         }
     }
 
+    static void Combi(int num) {
+        temp
+    }
 
-    static void moveLine() {
-        int[][] tmp = new int[n][n];
-        ArrayDeque<Integer> integers = new ArrayDeque<>();
+    static String isLenTwo(String s) {
+        int first = Integer.parseInt(String.valueOf(s.charAt(0)));
+        int second = Integer.parseInt(String.valueOf(s.charAt(1)));
+        return String.valueOf(first + second);
+    }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[j][i] == 0) continue;
-                integers.add(Main.arr[j][i]);
+    static int calHolsu(String s) {
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((Integer.parseInt(String.valueOf(s.charAt(i))) % 2) == 1) {
+                cnt++;
             }
         }
-        int tmpN = 0;
-        while (!integers.isEmpty()) {
-            tmp[0][tmpN++] = integers.poll();
-        }
-        arr = tmp;
+        return cnt;
     }
 
-    static void moveHarp() {
-        int moveLine = n / 2;
 
-        for (int i = 0; i < moveLine; i++) {
-            arr[1][moveLine - i - 1] = arr[0][i];
-            arr[0][i] = arr[0][i + moveLine];
-        }
-
-        for (int i = moveLine; i < n; i++) {
-            arr[0][i] = 0;
-        }
-        moveLine /= 2;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < moveLine; j++) {
-                arr[1 + 2 - i][moveLine - j - 1] = arr[i][j];
-                arr[i][j] = arr[i][j + moveLine];
-            }
-        }
-
-        for (int i = moveLine; i < n; i++) {
-            arr[0][i] = 0;
-            arr[1][i] = 0;
-        }
-    }
-
-    static boolean checkFinish() {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            if (max < arr[0][i]) {
-                max = arr[0][i];
-            }
-            if (min > arr[0][i]) {
-                min = arr[0][i];
-            }
-        }
-        return (max - min) > k;
-    }
-
-    static boolean checkSize(int y, int x) {
-        return 0 <= y && y < n && 0 <= x && x < n;
-    }
-
-    static void print() {
-        for (int[] ints : arr) {
-            System.out.println(Arrays.toString(ints));
-        }
-        System.out.println();
-    }
 }
-
