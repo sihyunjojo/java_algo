2604b0d00f9c4535efb7d0ca0b396653abc31d6c
sihyunjojo <si4018@naver.com>
2024-05-08 20:37:36 +0900

    240508

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 //수열과 쿼리 17 실패
 public class Main {
+<<<<<<< Updated upstream
     static int res, n;
     static int[] arr, tree;
     static StringBuilder sb = new StringBuilder();
@@ -58,4 +59,60 @@ public class Main {
         return (int) Math.pow(2, h) - 1;
     }
 
+=======
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static class Planet {
+        int radius;
+        Point point;
+
+        public Planet(int x, int y, int radius) {
+            this.radius = radius;
+            this.point = new Point(x, y);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int tc = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < tc; i++) {
+            int[] arr = getIntArrFromStr(br.readLine());
+            Point startPoint = new Point(arr[0], arr[1]);
+            Point endPoint = new Point(arr[2], arr[3]);
+            int num = Integer.parseInt(br.readLine());
+            int res = 0;
+                
+            for (int j = 0; j < num; j++) {
+                int[] planetArr = getIntArrFromStr(br.readLine());
+                Planet planet = new Planet(planetArr[0], planetArr[1], planetArr[2]);
+                if (isPointInPlanet(startPoint, planet) ^ isPointInPlanet(endPoint, planet)) {
+                    res++;
+                }
+            }
+            sb.append(res).append("\n");
+        }
+        System.out.println(sb);
+    }
+    
+
+    static int[] getIntArrFromStr(String str) {
+        return Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    static boolean isPointInPlanet(Point point, Planet planet) {
+        Point planetPoint = planet.point;
+        double distance = Math.sqrt(Math.pow(planetPoint.x - point.x, 2) + Math.pow(planetPoint.y - point.y, 2));
+        return distance <= planet.radius;
+    }
+>>>>>>> Stashed changes
 }
